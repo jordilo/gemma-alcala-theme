562863a443611f731ed65cc0b76c6fd575e58592
jordi lopez galera <jordi8284@gmail.com>
2020-04-18 09:10:25 +0200

    improve gallery sight

--- gallery.php
+++ gallery.php
@@ -2,10 +2,10 @@
     <?php foreach (array_filter($exploded) as $mediaId) {?>
     <?php $mediaInfo = get_post($mediaId);?>
     <a class="example-image-link col-md-3"
-    style="background-image: url('<?=wp_get_attachment_thumb_url($mediaId)?>')"
         href="<?=wp_get_attachment_url($mediaId)?>"
         data-lightbox="portfolio-set"
         data-title="<?=$mediaInfo->post_title?> </br> <span style='font-weight: 200'><?=$mediaInfo->post_content?></span>">
+        <div class="example-image-link-bg"style="background-image: url('<?=wp_get_attachment_thumb_url($mediaId)?>')"></div>
         <div class="example-image-title"><div><?=$mediaInfo->post_title?> </br> <?=$mediaInfo->post_content?></div></div>
     </a>
     <?php }?>
