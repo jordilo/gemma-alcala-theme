305b7a4e6cd528555157bfd80926189f49a89e34
jordi lopez galera <jordi8284@gmail.com>
2020-09-05 09:03:26 +0200

    fix error in upload-gallery

--- js/upload-gallery.js
+++ js/upload-gallery.js
@@ -1,79 +1,85 @@
 var meta_gallery_frame;
 jQuery(document).ready(function($) {
     // Runs when the image button is clicked.
-    jQuery("#sortable").sortable({
-        update: function(event, ui) {
-            var metadataString = [];
-
-            var metadataString = [];
-            jQuery("#sortable li").each(function(el, el2) {
-                var imageId = jQuery(el2).find("img").attr("id");
-                metadataString.push(imageId);
-            })
-            jQuery("#portfolio_gallery_value").val(metadataString.toString());
-
-        }
-    });
-    jQuery("#portfolio_gallery_button").click(function(e) {
-        //Attachment.sizes.thumbnail.url/ Prevents the default action from occuring.
-        e.preventDefault();
-
-        // If the frame already exists, re-open it.
-        if (meta_gallery_frame) {
-            meta_gallery_frame.open();
-            return;
-        }
-
-        // Sets up the media library frame
-        meta_gallery_frame = wp.media.frames.meta_gallery_frame = wp.media({
-            title: "Select a gallery",
-            // button: { text: 'click' },
-            // library: { type: "image" },
-            multiple: true
-        });
-
-        // Create Featured Gallery state. This is essentially the Gallery state, but selection behavior is altered.
-        meta_gallery_frame.states.add([
-            new wp.media.controller.Library({
-                id: "portfolio-gallery",
-                title: "Select Images for Gallery",
-                priority: 20,
-                toolbar: "main-gallery",
-                filterable: "uploaded",
-                library: wp.media.query(meta_gallery_frame.options.library),
-                multiple: meta_gallery_frame.options.multiple ? "reset" : false,
-                editable: true,
-                allowLocalEdits: true,
-                displaySettings: true,
-                displayUserSettings: true
-            })
-        ]);
-
-        meta_gallery_frame.on("open", function() {
-            var selection = meta_gallery_frame.state().get("selection");
-            var library = meta_gallery_frame.state("gallery-edit").get("library");
-            var ids = jQuery("#portfolio_gallery_value").val();
-            if (ids) {
-                idsArray = ids.split(",");
-                idsArray.forEach(function(id) {
-                    attachment = wp.media.attachment(id);
-                    attachment.fetch();
-                    selection.add(attachment ? [attachment] : []);
-                });
+    var sortable = jQuery("#sortable");
+    if(sortable){
+        sortable.sortable({
+            update: function(event, ui) {
+                var metadataString = [];
+    
+                var metadataString = [];
+                jQuery("#sortable li").each(function(el, el2) {
+                    var imageId = jQuery(el2).find("img").attr("id");
+                    metadataString.push(imageId);
+                })
+                jQuery("#portfolio_gallery_value").val(metadataString.toString());
+    
             }
         });
-
-        meta_gallery_frame.on("ready", function() {
-            jQuery(".media-modal").addClass("no-sidebar");
+    }
+    var galleryButton = jQuery("#portfolio_gallery_button");
+    if(galleryButton){
+        galleryButton.click(function(e) {
+            //Attachment.sizes.thumbnail.url/ Prevents the default action from occuring.
+            e.preventDefault();
+    
+            // If the frame already exists, re-open it.
+            if (meta_gallery_frame) {
+                meta_gallery_frame.open();
+                return;
+            }
+    
+            // Sets up the media library frame
+            meta_gallery_frame = wp.media.frames.meta_gallery_frame = wp.media({
+                title: "Select a gallery",
+                // button: { text: 'click' },
+                // library: { type: "image" },
+                multiple: true
+            });
+    
+            // Create Featured Gallery state. This is essentially the Gallery state, but selection behavior is altered.
+            meta_gallery_frame.states.add([
+                new wp.media.controller.Library({
+                    id: "portfolio-gallery",
+                    title: "Select Images for Gallery",
+                    priority: 20,
+                    toolbar: "main-gallery",
+                    filterable: "uploaded",
+                    library: wp.media.query(meta_gallery_frame.options.library),
+                    multiple: meta_gallery_frame.options.multiple ? "reset" : false,
+                    editable: true,
+                    allowLocalEdits: true,
+                    displaySettings: true,
+                    displayUserSettings: true
+                })
+            ]);
+    
+            meta_gallery_frame.on("open", function() {
+                var selection = meta_gallery_frame.state().get("selection");
+                var library = meta_gallery_frame.state("gallery-edit").get("library");
+                var ids = jQuery("#portfolio_gallery_value").val();
+                if (ids) {
+                    idsArray = ids.split(",");
+                    idsArray.forEach(function(id) {
+                        attachment = wp.media.attachment(id);
+                        attachment.fetch();
+                        selection.add(attachment ? [attachment] : []);
+                    });
+                }
+            });
+    
+            meta_gallery_frame.on("ready", function() {
+                jQuery(".media-modal").addClass("no-sidebar");
+            });
+    
+            // When an image is selected, run a callback.
+            //meta_gallery_frame.on('update', function() {
+            meta_gallery_frame.on("select", updateGallery);
+    
+            // Finally, open the modal
+            meta_gallery_frame.open();
         });
-
-        // When an image is selected, run a callback.
-        //meta_gallery_frame.on('update', function() {
-        meta_gallery_frame.on("select", updateGallery);
-
-        // Finally, open the modal
-        meta_gallery_frame.open();
-    });
+    }
 
     jQuery(document.body).on("click", ".shift8_portfolio_gallery_close", function(
         event
